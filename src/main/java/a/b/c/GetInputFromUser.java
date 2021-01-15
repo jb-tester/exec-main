9cb801e5a1b1a54b82b5463dcdd609a6535ae794
Irina.Petrovskaya <irina.petrovskaya@jetbrains.com>
2021-01-15 12:28:16 +0300

    minor

--- src/main/java/a/b/c/GetInputFromUser.java
+++ src/main/java/a/b/c/GetInputFromUser.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 class GetInputFromUser 
 { 
-    public static void main(String args[]) 
-    { 
-        // Using Scanner for Getting Input from User 
+    public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
         System.out.println("enter string: ");
-        String s = in.nextLine(); 
-        System.out.println("You entered string "+s);
+        String s = in.nextLine();
+        System.out.println("You entered string " + s);
         System.out.println("enter integer: ");
-        int a = in.nextInt(); 
-        System.out.println("You entered integer "+a);
+        int a = in.nextInt();
+        System.out.println("You entered integer " + a);
         System.out.println("enter float: ");
         float b = in.nextFloat(); 
         System.out.println("You entered float "+b); 
